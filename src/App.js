e26e6c71fccae338ce423e602c4954fe11fccf61
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-02-24 15:50:09 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ class App extends React.Component {
       attr3: '',
       rare: 'normal',
       trunfo: false,
-      saveButtonDisable: true,
     };
   }
 
@@ -28,12 +27,50 @@ class App extends React.Component {
   }
 
   handleSave = () => {
-    console.log('oieee');
+    this.setState({
+      name: '',
+      description: '',
+      image: '',
+      attr1: '',
+      attr2: '',
+      attr3: '',
+      rare: 'normal',
+      trunfo: false,
+    });
+  }
+
+  handleSaveButton = () => {
+    const { name, description, image, attr1, attr2, attr3 } = this.state;
+    const max = 90;
+    const maxTotal = 210;
+    const soma = parseInt(attr1, 10) + parseInt(attr2, 10) + parseInt(attr3, 10);
+    if (!name.length) {
+      return true;
+    }
+    if (!description.length) {
+      return true;
+    }
+    if (!image.length) {
+      return true;
+    }
+    if (attr1 > max || attr1 < 0) {
+      return true;
+    }
+    if (attr2 > max || attr2 < 0) {
+      return true;
+    }
+    if (attr3 > max || attr3 < 0) {
+      return true;
+    }
+    if (soma > maxTotal) {
+      return true;
+    }
+    return false;
   }
 
   render() {
     const { name, description, image, attr1,
-      attr2, attr3, rare, trunfo, saveButtonDisable } = this.state;
+      attr2, attr3, rare, trunfo } = this.state;
 
     return (
       <div>
@@ -46,7 +83,7 @@ class App extends React.Component {
           cardImage={ image }
           cardRare={ rare }
           cardTrunfo={ trunfo }
-          isSaveButtonDisabled={ saveButtonDisable }
+          isSaveButtonDisabled={ this.handleSaveButton() }
           onInputChange={ this.handleChange }
           onSaveButtonClick={ this.handleSave }
         />
